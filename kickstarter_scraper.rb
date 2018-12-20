7829e689d44cea7a1fcd7187a1a1db476a71c89b
Abi Raskin <abiraskin@gmail.com>
2018-12-20 02:53:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,10 @@
 require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   html = File.read('fixures/Kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-end
 
-open fixtures/kickstarter.html
+  binding.pry
+end
